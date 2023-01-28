f7d0ca17927ec36910895bff44f74c0fdc533f52
Ali Asger Habib <habibaliasger18@gmail.com>
2023-01-28 20:54:54 +0530

    Created Chapter 5

--- src/Trial.java
+++ src/Trial.java
@@ -1,15 +1,8 @@
 @SuppressWarnings("unused")
 public class Trial {
 	public static void main(String[] args) {
-		char b = 'A';
-		byte a = 12;
-		short c = 125;
-		int d = 2000;
-		b = d;
-		b = a;
-		b = c;
-		c = b;
-		a = b;
-		d = b;
+		char a = 'A';
+		char b = 'B';
+		System.out.println(a + b + " is the result of the multiplication.");
 	}
 }
