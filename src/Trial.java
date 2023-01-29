9e90fdd9dc108450df26b0a32acc82162abcf6ae
Ali Asger Habib <habibaliasger18@gmail.com>
2023-01-29 19:28:51 +0530

    Completed Section 3

--- src/Trial.java
+++ src/Trial.java
@@ -1,8 +1,8 @@
 @SuppressWarnings("unused")
 public class Trial {
 	public static void main(String[] args) {
-		char a = 'A';
-		char b = 'B';
-		System.out.println(a + b + " is the result of the multiplication.");
+		byte a = 10, b = 2;
+		byte d = 10 / 2;
+		
 	}
 }
