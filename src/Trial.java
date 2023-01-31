8c4cb1847ff68bd81e7090d0e294588a6aab5d2d
Ali Asger Habib <habibaliasger18@gmail.com>
2023-01-31 22:29:43 +0530

    Ground Breaking Revelation

--- src/Trial.java
+++ src/Trial.java
@@ -1,8 +1,8 @@
+import javax.lang.model.element.VariableElement;
+
 @SuppressWarnings("unused")
 public class Trial {
 	public static void main(String[] args) {
-		byte a = 10, b = 2;
-		byte d = 10 / 2;
 		
 	}
 }
--- src/com/habib/section3/Chapter2.java
+++ src/com/habib/section3/Chapter2.java
@@ -1,39 +1,24 @@
 package com.habib.section3;
 
+@SuppressWarnings("unused")
 public class Chapter2 {
-
-	public static void main(String[] args) {
-		System.out.println("The size of byte is: " + Byte.SIZE / 8);
-		System.out.println("The range of byte is: " + Byte.MIN_VALUE + " - " + Byte.MAX_VALUE);
-		System.out.println();
+	
+	public static void part1()
+	{
+		int a = 2147483647 + 1; // integer + integer gives an integer.
+//		int b = 2147483648; // Is not an integer
 		
-		System.out.println("The size of short is: " + Short.SIZE / 8);
-		System.out.println("The range of short is: " + Short.MIN_VALUE + " - " + Short.MAX_VALUE);
-		System.out.println();
-		
-		System.out.println("The size of int is: " + Integer.SIZE / 8);
-		System.out.println("The range of int is: " + Integer.MIN_VALUE + " - " + Integer.MAX_VALUE);
-		System.out.println();
-		
-		System.out.println("The size of long is: " + Long.SIZE / 8);
-		System.out.println("The range of long is: " + Long.MIN_VALUE + " - " + Long.MAX_VALUE);
-		System.out.println();
-		
-		System.out.println("The size of float is: " + Float.SIZE / 8);
-		System.out.println("The range of float is: " + Float.MIN_VALUE + " - " + Float.MAX_VALUE);
-		System.out.println();	
-		
-		System.out.println("The size of double is: " + Double.SIZE / 8);
-		System.out.println("The range of double is: " + Double.MIN_VALUE + " - " + Double.MAX_VALUE);
-		System.out.println();
-		
-		System.out.println("The size of char is: " + Character.SIZE / 8);
-		System.out.println("The range of char is: " + Character.MIN_VALUE + " - " + Character.MAX_VALUE);
-		System.out.println("The range of char is: " + (int)Character.MIN_VALUE + " - " + (int)Character.MAX_VALUE);
-		System.out.println();
-		
+//	integer + integer gives an integer. But to assign an int to a byte, assignment narrowing takes place.:
+//  Rules: 
+//	1. Variable should be byte, short or char.	
+//	2. Should be inside the range of the variable type.
+// 		byte d = 127 + 1;
+		byte e = 125 + 1;	
+	}
+	
+	public static void main(String[] args) 
+	{
 		char myUnicode='\u0040';
 		System.out.println(myUnicode);		
-
 	}
 }
