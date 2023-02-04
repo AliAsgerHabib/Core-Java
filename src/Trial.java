6c448abe548cb1d92880a122efef563efba5e5b7
Ali Asger Habib <habibaliasger18@gmail.com>
2023-02-04 15:40:27 +0530

    Wrapped Up Section 4

--- src/Trial.java
+++ src/Trial.java
@@ -1,8 +1,12 @@
-import javax.lang.model.element.VariableElement;
-
 @SuppressWarnings("unused")
 public class Trial {
-	public static void main(String[] args) {
-		System.out.println('A' + 'B' + "is the string" + 'C' + 'D');
+	public static void main(String[] args) 
+	{
+		double num1 = 20.0;
+		double num2 = 80.0;
+		boolean isRemainderZero = (num1 + num2) * 100 % 40 == 0;
+		System.out.println(isRemainderZero);
+		if(!isRemainderZero)
+			System.out.println("Got some Remainder");
 	}
 }
---
+++ src/com/habib/section3/Chapter5.java
@@ -0,0 +1,19 @@
+package com.habib.section3;
+
+public class Chapter5 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		boolean check = false;
+		if(check && a++ > 2)
+			System.out.println(a);
+		System.out.println(a);
+		
+		check = true;
+		if(check || a++ > 2)
+			System.out.println(a);
+		System.out.println(a);
+	}
+
+}
