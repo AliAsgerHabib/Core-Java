701be1b755ffbfceca958fce1e9dde85e12f4e81
Ali Asger Habib <habibaliasger18@gmail.com>
2023-02-04 17:01:00 +0530

    Created Section 5

---
+++ src/com/habib/section5/Chapter1.java
@@ -0,0 +1,14 @@
+package com.habib.section5;
+
+public class Chapter1 {
+	 
+	void part1()
+	{
+		int  a = 10;
+		boolean isOver = false;
+		for(int i = 0; i < 10; i++)
+		{
+			int a = 20;
+		} 
+	 }
+}
