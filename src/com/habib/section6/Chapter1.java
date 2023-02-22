d2c44d7848e8026243964fae84fda9c988a29ddb
Ali Asger Habib <habibaliasger18@gmail.com>
2023-02-22 23:54:13 +0530

    Worked on Section 6

---
+++ src/com/habib/section6/Chapter1.java
@@ -0,0 +1,45 @@
+package com.habib.section6;
+
+@SuppressWarnings("unused")
+public class Chapter1 {
+	
+	public static void part1()
+	{
+		final int var = 3;
+		int a = 2, b = 25;
+		
+		switch(a)
+		{
+		case 1 : System.out.println("One"); break; // Works for a literal
+		case 1 + 1  : System.out.println("Two"); break; // Works for a constant expression
+		case var : System.out.println("Three"); break; // Works for final variable
+//		case 2 : System.out.println("Pair"); break; // Does not work for duplicate values
+//		case "Sherlock" : System.out.println("Pair"); break; // Type incompatible
+		}
+	}
+	
+	public static void part2()
+	{
+		String day = "Wednesday";
+
+		double dayNumber = 
+			switch(day)
+		{
+		case "Monday" -> 1.2; 
+		case "Tuesday" , "tuesday" -> 2;
+		case "Wednesday"-> 
+			{
+				System.out.println("I will eat an egg today");
+				yield 3;
+			}
+//		case "Thursday -> yield 4; // Error : yield works only in a code block
+		case "Thursday"-> 
+			{
+				yield 4;
+			}
+		default -> -1;
+		};
+				
+		System.out.println(dayNumber);
+	}
+}
---
+++ src/com/habib/section6/Chapter2.java
@@ -0,0 +1,73 @@
+package com.habib.section6;
+
+@SuppressWarnings("unused")
+public class Chapter2 {
+
+	public static void part1()
+	{
+		int i = 3;
+		l1:
+		{
+			System.out.println("Inside 1");
+		}
+		l2:
+		{
+			System.out.println("Before 2");
+			l3:
+			{
+				System.out.println("Before 3");
+//				if(i == 1) break l1; // Cannot break to l1 as it is does not enclose the break statement
+				if(i == 2) break l2; // Breaks out of the block of code labeled b2
+				else if(i == 3) break l3; // Breaks out of the block of code labeled b3
+				System.out.println("After 3");
+			}
+			System.out.println("After 2");
+		}
+		System.out.println("Always executes");
+	}
+	
+	public static void part2()
+	{
+		int check;
+		
+		check = 4; // Set check = 1 | 2 | 3 | 4
+		
+		System.out.println("Before the i loop");
+		ol:
+		for(int i = 0 ; i < 4; i++)ola:
+		{
+			System.out.println("Before the j loop");
+			il:
+			for(int j = 0; j < 4; j++)ila:
+			{
+				System.out.println("Before the break --- " + i + j);
+				if (check == 1 && j == 2) break ol;
+				else if (check == 2 && j == 2) break il;
+				else if (check == 3 && j == 2) break ila; // Point No 3. 
+				else if (check == 4 && j == 2) break ola; // Point No 3.
+				System.out.println("After the break --- " + i + j);
+			}
+			System.out.println("After the j loop");
+		}
+		System.out.println("After the i loop");
+	}
+
+	public static void part3()
+	{
+		char check = 'y'; // Set check = x | y
+		
+		x:
+		for(int i = 0; i < 5 ; i++)
+		{
+			y:
+			for (int j = 0; j < 5 ; j++)
+			{
+				if(j == 2 && check == 'x') continue x;
+				else if (j == 2 && check == 'y') continue y;
+				System.out.print(i + ":" + j + " ");
+			}
+			System.out.println("\n end of i loop");
+		}
+	}
+
+}
